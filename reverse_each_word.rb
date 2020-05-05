9320c6ca46f28148d9cbf12f180cea899cdb2458
Kevin Boston <kbos22@gmail.com>
2020-05-05 17:50:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   #convert into Array
-   new_arr = sentence.split(//)
+   new_arr = sentence.split(/ /)
   #map to itterate over Array
   new_arr.collect do |word|
     word.reverse
